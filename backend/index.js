d24fcfe36147810facfcca2c529d9d198d3157c4
mhit30 <mohammedhit30@gmail.com>
2025-05-23 19:19:47 -0700

    setup port

--- backend/index.js
+++ backend/index.js
@@ -259,7 +259,7 @@ io.on("connection", async (socket) => {
 
 mongoose.connect(process.env.MONGO_URI_PROD).then(() => {
   console.log("MongoDB connected");
-  server.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.CLIENT_URL}`);
+  server.listen(process.env.PORT || 5001, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
   });
 });

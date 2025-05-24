a2e1f147bdab1ee6e182e3370bafb784c0f07c33
mhit30 <mohammedhit30@gmail.com>
2025-05-23 19:01:37 -0700

    ready for prod

--- backend/index.js
+++ backend/index.js
@@ -257,7 +257,7 @@ io.on("connection", async (socket) => {
   });
 });
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+mongoose.connect(process.env.MONGO_URI_PROD).then(() => {
   console.log("MongoDB connected");
   server.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.CLIENT_URL}`);
